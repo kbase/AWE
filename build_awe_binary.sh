9cbffc97c0673b0aed2d9828730c1566d0502b58
Steve Chan <sychan168@gmail.com>
2017-08-10 14:54:58 -0700

    Make it all run as a single run-on sentence...

--- build_awe_binary.sh
+++ build_awe_binary.sh
@@ -8,18 +8,18 @@
 export GOPATH=/tmp/goroot
 DEST=`pwd`
 
-mkdir -p $GOPATH/src/github.com/MG-RAST
-go get github.com/pborman/uuid
-go get github.com/MG-RAST/golib
-go get github.com/MG-RAST/go-dockerclient
-cd $GOPATH/src/github.com/MG-RAST
-git clone --recursive https://github.com/kbase/AWE
-cd AWE
-git checkout $COMMIT
-cd awe-server
-go build
-cp awe-server /kb/deployment/bin/
-cd ../awe-client
-go build
-cp awe-client /kb/deployment/bin/
+mkdir -p $GOPATH/src/github.com/MG-RAST && \
+go get github.com/pborman/uuid && \
+go get github.com/MG-RAST/golib && \
+go get github.com/MG-RAST/go-dockerclient && \
+cd $GOPATH/src/github.com/MG-RAST && \
+git clone --recursive https://github.com/kbase/AWE && \
+cd AWE && \
+git checkout $COMMIT && \
+cd awe-server && \
+go build && \
+cp awe-server /kb/deployment/bin/ && \
+cd ../awe-client && \
+go build && \
+cp awe-client /kb/deployment/bin/ && \
 echo AWE server and client binaries copied to /kb/deployment/bin
--- build_awe_image.sh
+++ build_awe_image.sh
@@ -3,7 +3,8 @@
 export BRANCH=${TRAVIS_BRANCH:-`git symbolic-ref --short HEAD`}
 export DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 export COMMIT=${TRAVIS_COMMIT:-`git rev-parse --short HEAD`}
-docker run -v `pwd`/deployment:/kb/deployment -v `pwd`/build_awe_binary.sh:/tmp/build_awe_binary.sh -e COMMIT=$COMMIT kbase/kb_go /tmp/build_awe_binary.sh
+docker run -v `pwd`/deployment:/kb/deployment -v `pwd`/build_awe_binary.sh:/tmp/build_awe_binary.sh \
+	   -e COMMIT=$COMMIT kbase/kb_go /tmp/build_awe_binary.sh && \
 docker build --build-arg BUILD_DATE=$DATE \
    			 --build-arg VCS_REF=$COMMIT \
 			 --build-arg BRANCH=$BRANCH \
